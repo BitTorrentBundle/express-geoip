c47e06c7310825a55dc91006cacfc22a1768d951
Patrick Williams <pwmckenna@gmail.com>
2015-04-15 11:35:36 -0700

    cluster

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 'use strict';
 
-var assert = require('assert');
-var express = require('express');
-var app = express();
+var cluster = require('cluster');
 
-var ip = require('ip');
-var geoip = require('geoip-lite');
+if (cluster.isMaster) {
+    for (var i = 0; i < require('os').cpus().length; ++i) {
+        cluster.fork();
+    }
+} else {
+    var assert = require('assert');
+    var express = require('express');
+    var app = express();
 
-app.use(function (req, res, next) {
-    var geo = geoip.lookup(ip.isPrivate(req.ip) ? '8.8.8.8' : req.ip);
-    console.log('geo', geo);
-    res.status(200).json(geo);
-});
+    var ip = require('ip');
+    var geoip = require('geoip-lite');
 
-app.use(function (req, res, next) {
-    res.send(404);
-});
+    app.use(function (req, res, next) {
+        var geo = geoip.lookup(ip.isPrivate(req.ip) ? '8.8.8.8' : req.ip);
+        console.log('geo', geo);
+        res.status(200).json(geo);
+    });
 
-app.use(function (err, req, res, next) {
-    res.send(500);
-});
+    app.use(function (req, res, next) {
+        res.send(404);
+    });
 
-assert(process.env.PORT, 'process.env.PORT required');
-app.listen(process.env.PORT, function (err) {
-    console.log('listening to port ' + process.env.PORT, err);
-});
+    app.use(function (err, req, res, next) {
+        res.send(500);
+    });
+
+    assert(process.env.PORT, 'process.env.PORT required');
+    app.listen(process.env.PORT, function (err) {
+        console.log('listening to port ' + process.env.PORT, err);
+    });
+}
